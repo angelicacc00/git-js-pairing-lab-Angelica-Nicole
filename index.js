aac345cf11d9e8787992f044757309a80a2e2154
angelicacc00 <angelicac0721@gmail.com>
2023-10-04 14:00:20 -0400

    Function 3 added

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 //Code your solutions in this file
+
+//Function 1
 const fiveToOneHundred = () => {
     for(let i =5; i <= 100; i++) {
         console.log(i);
@@ -7,6 +9,7 @@ const fiveToOneHundred = () => {
 //Test 
 //console.log(fiveToOneHundred());
 
+//Function 2
 const multiplesOfThree = () => {
     for(let i = 1; i <= 100; i++) {
         if (i % 3 === 0){
@@ -15,4 +18,17 @@ const multiplesOfThree = () => {
     };
 };
 
-multiplesOfThree();
+//Test
+//multiplesOfThree();
+
+//Function 3
+const multiplesOfThreeOrFive = () => {
+    for(let i = 1; i <= 100; i++){
+        if ((i % 3 === 0)  || (i % 5 === 0)){
+            console.log(i)
+        };
+    };
+};
+
+//Test 
+multiplesOfThreeOrFive();
