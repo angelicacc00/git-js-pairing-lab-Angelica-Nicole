61f53e0876cc5c663a9cf19d456edfb913d106c3
cyph404 <nicoleblanchette05@gmail.com>
2023-10-04 14:37:31 -0400

    Added function 10

--- index.js
+++ index.js
@@ -85,3 +85,17 @@ const betweenTwentyAndForty = num =>{
 
 //Test 
 //console.log(betweenTwentyAndForty(35));
+
+//Function 10
+const largest = (num1, num2, num3) => {
+    if (num1 >= num2 && num1 >= num3){
+        return num1;
+    } else if (num2 >= num3){
+        return num2;
+    } else {
+        return num3;
+    };
+};
+
+//Test
+console.log(largest(4, 6, 8));
