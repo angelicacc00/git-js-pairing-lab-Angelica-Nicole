5d7c8d40ecc21ee8ba7142a5157e6351d65c2185
cyph404 <nicoleblanchette05@gmail.com>
2023-10-05 20:30:17 -0400

    Deleted placeholder comment

--- index.js
+++ index.js
@@ -123,10 +123,6 @@ const isLeapYear = year => {
 //console.log(isLeapYear(1999));
 
 //13
-//Write a function absoluteNineteen to compute the absolute difference 
-//between a specified number and 19. Returns triple their absolute difference 
-//if the specified number is greater than 19.
-
 const getExtension = filename => {
     return filename.slice(filename.indexOf('.'));
 };
